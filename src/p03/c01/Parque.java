04c3a3ac17cfec335924a7a2e8d8123dcf0a6c37
jjd0001 <jjd0001@alu.ubu.es>
2021-04-10 20:34:46 +0200

    modificación de los métodos de espera

--- src/p03/c01/Parque.java
+++ src/p03/c01/Parque.java
@@ -37,15 +37,15 @@ public class Parque implements IParque{
 
 	@Override
 	public synchronized void entrarAlParque(String puerta){		
-	
+		//Comprobamos si no sobrepasa el limite de personas
+		comprobarAntesDeEntrar(puerta);
+		
+		
 		// Si no hay entradas por esa puerta, inicializamos
 		if (contadoresPersonasPuerta.get(puerta) == null){ 
 			contadoresPersonasPuerta.put(puerta, 0);
 		}
-		
-		//Comprobamos si no sobrepasa el limite de personas
-		comprobarAntesDeEntrar(puerta);
-		
+	
 		//Calculamos el tiempo de entrada 
 		tEntrada = System.currentTimeMillis();		
 							
@@ -58,21 +58,22 @@ public class Parque implements IParque{
 		// Imprimimos el estado del parque
 		imprimirInfo(puerta, "Entrada");
 		
-		//Comprobamos el invariante
-		checkInvariante();
 		
 		notifyAll();
 		
+		//Comprobamos el invariante
+		checkInvariante();
 	}
 	
 	public synchronized void salirDelParque(String puerta){
+		//Comprobamos si no sobrepasa el limite
+		comprobarAntesDeSalir(puerta);
+		
 		// Si no hay salidas por esa puerta, inicializamos
 		if (contadoresPersonasPuerta.get(puerta) == null){ 
 			contadoresPersonasPuerta.put(puerta, 0);
 		}
 		
-		//Comprobamos si no sobrepasa el limite
-		comprobarAntesDeSalir(puerta);
 		
 		//Calcula el tiempo de entrada 
 		tSalida = System.currentTimeMillis();		
@@ -88,11 +89,11 @@ public class Parque implements IParque{
 		
 		// Imprimimos el estado del parque
 		imprimirInfo(puerta, "Salida");
-					
-		//Comprobamos el invariante
-		checkInvariante();
 		
 		notifyAll();
+		
+		//Comprobamos el invariante
+		checkInvariante();
 	}
 	
 	
@@ -125,32 +126,15 @@ public class Parque implements IParque{
 	}
 
 	protected synchronized void comprobarAntesDeEntrar(String p){	// String p lo pongo yo					
-		/*try{				
-			assert contadoresPersonasPuerta.get(p) <= 20;				
-		}catch(AssertionError e){
-			System.out.println("Puerta " + p + " completa");
-		}	*/	
-		System.out.println("Intento entrar por puerta " + p);
-		while ( contadorPersonasTotales > 40 )
-			try { wait(); }
-			catch(InterruptedException e) {}
-		//assert contadorPersonasTotales <= 40 : "Parque completo";
+		while(contadorPersonasTotales == 40){
+	           try {
+				wait();
+			} catch (InterruptedException e) {}
+	        }
 	}
 
 	protected synchronized void comprobarAntesDeSalir(String p){		// String p lo pongo yo
-		
-			//Comprueba que existe la puerta y que su contador sea mayor que 0
-			//assert contadoresPersonasPuerta.get(p) != null && contadoresPersonasPuerta.get(p) > 0 : "Salida cancelada";			
-		/*
-			System.out.println("Salida cancelada");
-			System.out.println("Puerta " + p + " vacia");
-			System.out.println(" ");
-		}*/
-		System.out.println("Intento salir por puerta " + p);
-		//assert contadorPersonasTotales > 0 : "Parque vacio";
-		//assert contadoresPersonasPuerta.containsKey(p): "Puerta inexistente";
-		//assert contadorPersonasTotales > 0 : "Puerta vacia";
-		while ( contadorPersonasTotales < 0 )
+		while ( contadorPersonasTotales == 0 )
 			try { wait(); }
 			catch(InterruptedException e) {}
 	}
