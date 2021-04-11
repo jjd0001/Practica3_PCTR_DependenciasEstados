a5653359f3f0f4b49b5ba6eb7fa537fac7c37cdc
lcg0020 <57801031+lcg0020@users.noreply.github.com>
2021-04-11 11:13:07 +0200

    Cambios en las precondiciones y agrego comentarios

--- src/p03/c01/Parque.java
+++ src/p03/c01/Parque.java
@@ -6,11 +6,10 @@ import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import t02.pg02.c01.IParque;
+import p03.c01.IParque;
 
 
 public class Parque implements IParque{
-
 	
 	//Personas totales en el parque
 	private int contadorPersonasTotales; 
@@ -24,83 +23,77 @@ public class Parque implements IParque{
 	private long tSalida;
 	//Calculo del tiempo medio
 	private double tMedio;
-
-
 		
-	public Parque() {	
+	public Parque() {	//Constructor del parque
 		contadorPersonasTotales = 0;
 		contadoresPersonasPuerta = new Hashtable<String,Integer>();
 		tInicial = System.currentTimeMillis();
 		tMedio = 0;
 	}
 
-
 	@Override
 	public synchronized void entrarAlParque(String puerta){		
-		//Comprobamos si no sobrepasa el limite de personas
-		comprobarAntesDeEntrar(puerta);
 		
+		//Comprobamos si no sobrepasa el limite de personas
+		comprobarAntesDeEntrar();		
 		
 		// Si no hay entradas por esa puerta, inicializamos
 		if (contadoresPersonasPuerta.get(puerta) == null){ 
 			contadoresPersonasPuerta.put(puerta, 0);
-		}
-	
+		}	
+		
 		//Calculamos el tiempo de entrada 
-		tEntrada = System.currentTimeMillis();		
-							
+		tEntrada = System.currentTimeMillis();									
+		
 		// Aumentamos el contador total y el individual
 		contadorPersonasTotales++;		
-		contadoresPersonasPuerta.put(puerta, contadoresPersonasPuerta.get(puerta)+1);
-	
-		
+		contadoresPersonasPuerta.put(puerta, contadoresPersonasPuerta.get(puerta)+1);		
 		
 		// Imprimimos el estado del parque
-		imprimirInfo(puerta, "Entrada");
+		imprimirInfo(puerta, "Entrada");		
 		
-		
-		notifyAll();
+		//Notificamos al resto de hilos
+		notifyAll();		
 		
 		//Comprobamos el invariante
 		checkInvariante();
 	}
 	
 	public synchronized void salirDelParque(String puerta){
-		//Comprobamos si no sobrepasa el limite
-		comprobarAntesDeSalir(puerta);
 		
+		//Comprobamos si no sobrepasa el limite
+		comprobarAntesDeSalir();		
 		// Si no hay salidas por esa puerta, inicializamos
+		
 		if (contadoresPersonasPuerta.get(puerta) == null){ 
 			contadoresPersonasPuerta.put(puerta, 0);
 		}
 		
-		
-		//Calcula el tiempo de entrada 
-		tSalida = System.currentTimeMillis();		
+		//Calculamos el tiempo de entrada 
+		tSalida = System.currentTimeMillis();				
 		
 		// Decrementamos el contador total y el individual
 		contadorPersonasTotales--;		
-		contadoresPersonasPuerta.put(puerta, contadoresPersonasPuerta.get(puerta)-1);
-		
-		// Si la puerta se queda a 0, se elimina ¿por qué?
-		/*if (contadoresPersonasPuerta.get(puerta) == 0){
-			contadoresPersonasPuerta.remove(puerta);
-		}*/
+		contadoresPersonasPuerta.put(puerta, contadoresPersonasPuerta.get(puerta)-1);		
 		
 		// Imprimimos el estado del parque
 		imprimirInfo(puerta, "Salida");
 		
-		notifyAll();
+		//Notificamos al resto de hilos
+		notifyAll();		
 		
 		//Comprobamos el invariante
 		checkInvariante();
 	}
-	
-	
-	private void imprimirInfo (String puerta, String movimiento){
 		
+	private void imprimirInfo (String puerta, String movimiento){	
+		
+		//Imprimimos el movimiento y las personas que hay en el parque
 		System.out.println(movimiento + " por puerta " + puerta);
-		System.out.println("--> Personas en el parque " + contadorPersonasTotales + " tiempo medio de estancia: "  + obtenerTmedio());
+		if (movimiento =="Entrada")
+			System.out.println("--> Personas en el parque " + contadorPersonasTotales);
+		else //Imprimimos el tiempo medio a la salida
+			System.out.println("--> Personas en el parque " + contadorPersonasTotales + " tiempo medio de estancia: "  + obtenerTmedio());		
 		
 		// Iteramos por todas las puertas e imprimimos sus entradas
 		for(String p: contadoresPersonasPuerta.keySet()){
@@ -110,6 +103,7 @@ public class Parque implements IParque{
 	}
 	
 	private int sumarContadoresPuerta() {
+		//Acumulamos los contadores de cada puerta
 		int sumaContadoresPuerta = 0;
 			Enumeration<Integer> iterPuertas = contadoresPersonasPuerta.elements();
 			while (iterPuertas.hasMoreElements()) {
@@ -118,31 +112,32 @@ public class Parque implements IParque{
 		return sumaContadoresPuerta;
 	}
 	
-	protected void checkInvariante() {
-		
+	protected void checkInvariante() {		
+		//Comprobamos que la suma de los contadores de las puertas sea igual a la suma de presonas totales
 		assert sumarContadoresPuerta() == contadorPersonasTotales : "INV: La suma de contadores de las puertas debe ser igual al valor del contador del parque";
 		assert contadorPersonasTotales >= 0 : "INV: El parque no puede tener menos de 0 personas";
-
 	}
 
-	protected synchronized void comprobarAntesDeEntrar(String p){	// String p lo pongo yo					
-		while(contadorPersonasTotales == 40){ //Deberia de comprobar que la puerta no este llena (20)
+	protected synchronized void comprobarAntesDeEntrar(){	
+		//Cuando se alcanza el limite del parque, el hilo se pone a la espera
+		while(contadorPersonasTotales == 40 ){ 
 	           try {
-				wait();
+				wait(); 
 			} catch (InterruptedException e) {}
 	        }
 	}
 
-	protected synchronized void comprobarAntesDeSalir(String p){		// String p lo pongo yo
-		while ( contadorPersonasTotales == 0 ) //Deberia de comprobar que la puerta no esta vacia	
-			try { wait(); }
-			catch(InterruptedException e) {}
+	protected synchronized void comprobarAntesDeSalir(){
+		//Cuando no hay nadie en el parque, el hilo se pone a la espera
+		while (contadorPersonasTotales == 0 ) 	
+			try { wait(); } 
+			catch(InterruptedException e) {}	
 	}
 	
 	private double obtenerTmedio() {
-		tMedio = (tMedio + (tEntrada - tSalida))/2.0; 
+		//Calculamos el tiempo medio de estancia
+		tMedio = (tMedio + (tSalida - tEntrada))/2.0; 
 		return tMedio; 
 	}
-
 
 }
